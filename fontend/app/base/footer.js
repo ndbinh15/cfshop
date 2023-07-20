519c225f6592a008decb26137beeba5bf2fe6141
ndbinh <truongbinh15a@gmail.com>
2023-07-21 00:42:58 +0700

    Update header and footer template and component

---
+++ fontend/app/base/footer.js
@@ -0,0 +1,9 @@
+angular.module('cfshop')
+  .component('footerComponent', {
+    templateUrl: function () {
+      return window.config.baseLocation + '/footer.html';
+    },
+    // controller: [function () {
+       
+    // }]
+});
---
+++ fontend/app/base/header.js
@@ -0,0 +1,18 @@
+angular.module('cfshop')
+  .directive('headerComponent', function() {
+    return {
+      templateUrl: function() {
+        return window.config.baseLocation + '/header.html';
+      },
+      scope: {
+        login: "="
+      },
+      controller: ['$scope', function($scope) {
+        $scope.isNavbarOpen = false;
+
+        $scope.toggleNavbar = function() {
+          $scope.isNavbarOpen = !$scope.isNavbarOpen;
+        };
+      }]
+    };
+  });
