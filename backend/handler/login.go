838c5c80140b1f020caec194ace6607459948808
ndbinh <truongbinh15a@gmail.com>
2023-07-21 00:37:08 +0700

    Update login logic

---
+++ backend/handler/login.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	// v, _ := hashPassword("abc")
+	// log.Println("Go hash pass")
+	// log.Println(v)
+
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	} else {
+		log.Println("$POST success")
+	}
+
+	var loginRequest LoginRequest
+	err := json.NewDecoder(r.Body).Decode(&loginRequest)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	username := loginRequest.Username
+	password := loginRequest.Password
+
+	log.Print(username)
+	log.Print(password)
+	err = AuthenticateUser(username, password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	// Redirect to the home page on successful login
+	// http.Redirect(w, r, "../fontend/home.html", http.StatusFound)
+}
