0f10a23146a3850ae0ad7592c006c0bd0986c8a1
ndbinh <truongbinh15a@gmail.com>
2023-07-21 00:45:01 +0700

    Update admin template and component

---
+++ fontend/app/controller/admin.js
@@ -0,0 +1,36 @@
+angular.module('cfshop')
+    .controller('adminController', ['$scope', function ($scope) {
+        const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+        const yValues = [55, 49, 44, 24, 55];
+        const barColors = ["red", "green", "blue", "orange", "brown"];
+
+        new Chart("myChart", {
+            type: "bar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: "World Wine Production 2018"
+                }
+            }
+        });
+
+        $scope.openNav = function () {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        };
+
+        $scope.closeNav = function () {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            document.body.style.backgroundColor = "white";
+        }
+    }]);
